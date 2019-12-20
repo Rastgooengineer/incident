cf44aa522076213efb648f2efe1d05d92459b64d
Rastgooengineer <elhamrastgoo@gmail.com>
2019-12-20 10:05:20 +0330

    Add files via upload

--- incidentrecord/incidents/views.py
+++ incidentrecord/incidents/views.py
@@ -1,11 +1,19 @@
 from django.http import Http404
 from django.shortcuts import render
-from .models import cam01_incidents_2019
+from .models import IncidentsInf
 def index(request):
-  #  return HttpResponse("Hello, world. You're at the polls index.")
+    videoname = IncidentsInf.objects.filter(check = 0)
+    for obj in videoname:
+        name = obj.video_name.split('_')[0]
+        year = str(obj.videodatetime).split('-')[0]
+        month =  str(obj.videodatetime).split('-')[1]
+        print (name , year , month)
+        obj.video_path = "{}/{}/{}".format(name,year,month)
+        obj.check = 1
+        obj.save()
+
     try:
-        allvideos = cam01_incidents_2019.objects.all()
-        print(allvideos)
-    except cam01_incidents_2019.DoesNotExist:
+        allvideos = IncidentsInf.objects.all()
+    except IncidentsInf.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'home.html',  {'allvideos': allvideos})
