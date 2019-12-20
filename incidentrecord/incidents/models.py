89cedc5f87d7a3b30edf4155eb75ec1f409713b4
Rastgooengineer <elhamrastgoo@gmail.com>
2019-12-20 10:02:57 +0330

    Add files via upload

--- incidentrecord/incidents/models.py
+++ incidentrecord/incidents/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-class cam01_incidents_2019(models.Model):
-    time = models.DateTimeField(auto_now_add=True, null=False)
+class IncidentsInf(models.Model):
+    videodatetime = models.DateTimeField(auto_now_add=True, null=False)
     name = models.CharField(max_length=50, null=False)
     type = models.CharField(max_length=100, null=False)
-    incident_avi = models.CharField(max_length=100, blank=True)
+    video_name = models.CharField(max_length=100, blank=True)
+    video_path = models.CharField(max_length=200,null=True, default= "/")
+    check = models.IntegerField()
     objects = models.Manager()
